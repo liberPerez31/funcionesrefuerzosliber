66f0fbcfed6b2787d5ee0e3eee893a7680d7f518
liberperez <perezlibertad52@gmail.com>
2024-06-24 19:04:00 -0400

    liberperez

---
+++ juego/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Progra
+{
+    static Random random = new Random();
+
+    static int LanzarDado()
+    {
+        return random.Next(1, 7); // Genera un número entre 1 y 6
+    }
+
+    static void Main()
+    {
+        bool continuar = true;
+
+        while (continuar)
+        {
+            int dado1 = LanzarDado();
+            int dado2 = LanzarDado();
+
+            Console.WriteLine($"Dado 1: {dado1} | Dado 2: {dado2}");
+
+            Console.Write("¿Quieres volver a lanzar? (s/n): ");
+            string respuesta = Console.ReadLine();
+
+            continuar = (respuesta.ToLower() == "s");
+        }
+
+        Console.WriteLine("¡Gracias por jugar!");
+    }
+}
---
+++ numerosP/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Programm
+{
+    static bool EsPrimo(int numero)
+    {
+        if (numero < 2)
+            return false;
+
+        for (int i = 2; i < numero; i++)
+        {
+            if (numero % i == 0)
+                return false; // No es primo
+        }
+
+        return true; // Es primo
+    }
+
+    static void Main()
+    {
+        Console.Write("Ingresa el número máximo del rango: ");
+        int maximo = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Números primos encontrados:");
+
+        for (int num = 2; num <= maximo; num++)
+        {
+            if (EsPrimo(num))
+                Console.Write(num + " ");
+        }
+
+        Console.WriteLine();
+    }
+}
